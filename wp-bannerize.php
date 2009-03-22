ca015ddb7c9effe2c240eda709991c9cef01c29d
gfazioli <gfazioli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-22 15:56:45 +0000

    1.3.1 - update jquery release
    git-svn-id: https://plugins.svn.wordpress.org/wp-bannerize/trunk@104227 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-bannerize.php
+++ wp-bannerize.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-BANNERIZE
 Plugin URI: http://wordpress.org/extend/plugins/wp-bannerize/
 Description: WP_BANNERIZE is a image banner manager. See <a href="options-general.php?page=wp-bannerize.php">configuration panel</a> for more settings. For more info and plugins visit <a href="http://labs.saidmade.com">Labs Saidmade</a>.
-Version: 1.3.0
+Version: 1.3.1
 Author: Giovambattista Fazioli
 Author URI: http://labs.saidmade.com
 Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
