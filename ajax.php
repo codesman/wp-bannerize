843afc95449a8b738f1c4b21385b622bef36dfe4
gfazioli <gfazioli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-17 13:57:03 +0000

    = 2.4.5.1 = * Add Secure Layer on Ajax Gateway
    git-svn-id: https://plugins.svn.wordpress.org/wp-bannerize/trunk@218695 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ajax.php
+++ ajax.php
@@ -8,7 +8,7 @@
  * @copyright       Copyright (C) 2010 Saidmade Srl
  * 
  */
-if ( isset($_SERVER['HTTP_X_REQUESTED_WITH']) ) {
+if ( @isset($_SERVER['HTTP_X_REQUESTED_WITH']) ) {
     require_once('../../../wp-config.php');
     $_db = @mysql_connect ( DB_HOST, DB_USER, DB_PASSWORD ); mysql_select_db( DB_NAME );
     foreach($_POST["item"] as $key => $value){
