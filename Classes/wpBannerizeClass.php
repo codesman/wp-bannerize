387d055f734fa542df33946eb33ed37e4d972e3d
gfazioli <gfazioli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-10 23:26:20 +0000

    = 3.0.0 = * Added insert banner by URL * Added insert banner by Free HTML * Added insert banner from Media Gallery * Added click count on Flash Movie * Added quick switch button to enable/disable banner * Added Server Date/Time information * Added `no_html_wrap` arguments for avoid WP Bannerize HTML * Added frontend stylesheet, predefined or custom * Added 'no banner to display' HTML/message settings * Added Tools menu with Function and Shortcode Editor, Database utility * Improved HTML contextual Help * Improved Code and HTML layout * Improved HTML output * Fixed shortcode * Fixed several minor bugs * Updated Fancybox Javascript Library * Deprecated shortcode name "wp-bannerize" * Deprecated HTML output - see documentation for detail
    git-svn-id: https://plugins.svn.wordpress.org/wp-bannerize/trunk@449665 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Classes/wpBannerizeClass.php
+++ Classes/wpBannerizeClass.php
@@ -2,20 +2,15 @@
 /**
  * Main class for sub.classing backend and frontend class
  *
- * @class			WPBANNERIZE_CLASS
- * @package         wp-bannerize
- * @subpackage      wp-bannerize_class
- * @author          =undo= <g.fazioli@saidmade.com>
+ * @class			WPBannerizeClass
+ * @package         wpBannerize
+ * @subpackage      wpBannerizeClass
+ * @author          =undo= <g.fazioli@undolog.com>, <g.fazioli@saidmade.com>
  * @copyright       Copyright © 2008-2011 Saidmade Srl
  *
  */
 
-define('WP_BANNERIZE_TABLE_2411', 	'bannerize');			///< Name of Database table up 2.4.11
-define('WP_BANNERIZE_TABLE_250', 	'bannerize_a');			///< Name of Database table fom 2.5.0
-define('WP_BANNERIZE_TABLE', 		'bannerize_b');			///< Name of Database table fom 2.7.0
-define('WP_BANNERIZE_OPTIONS',		'wp-bannerize');		///< Option Key @since 2.7.0.3
-
-class WPBANNERIZE_CLASS {
+class WPBannerizeClass {
 
 	/**
 	 * Plugin version (see above)
@@ -23,7 +18,7 @@ class WPBANNERIZE_CLASS {
 	 * @since 2.4.7
 	 * @var string
 	 */
-	var $version = "2.8.8";
+	var $version = kWPBannerizeVersion;
 
     /**
      * WP Bannerize release.minor.revision
@@ -42,7 +37,7 @@ class WPBANNERIZE_CLASS {
      * @since 1.0.0
      * @var string
      */
-    var $plugin_name = "WP Bannerize";
+    var $plugin_name = kWPBannerizePluginName;
 
     /**
      * Plugin slug
@@ -50,7 +45,7 @@ class WPBANNERIZE_CLASS {
      * @since 2.5.0
      * @var string
      */
-    var $plugin_slug = "wp-bannerize";
+    var $plugin_slug = kWPBannerizeSlugName;
 
     /**
      * Key for database options
@@ -58,7 +53,7 @@ class WPBANNERIZE_CLASS {
      * @since 1.0.0
      * @var string
      */
-    var $options_key = WP_BANNERIZE_OPTIONS;
+    var $options_key = kWPBannerizeOptionsKey;
 
     /**
      * Options array containing all options for this plugin
@@ -74,7 +69,7 @@ class WPBANNERIZE_CLASS {
      * @since 1.0.0
      * @var string
      */
-    var $options_title = "WP Bannerize";
+    var $options_title = kWPBannerizeOptionTitle;
 
     /**
      * Property for table name
@@ -87,8 +82,14 @@ class WPBANNERIZE_CLASS {
 
     var $content_url = "";
     var $plugin_url = "";
-    var $ajax_sorter = "";
-    var $ajax_clickcounter = "";
+
+	/**
+	 * Wordpress Ajax URL
+	 *
+	 * @since 3.0.0
+	 * @var string
+	 */
+	var $ajaxURL = '';
 
     var $path = "";
     var $file = "";
@@ -97,13 +98,15 @@ class WPBANNERIZE_CLASS {
     var $siteurl = "";
     var $wpadminurl = "";
 
+	var $error = false;
+
     /**
      * Standard PHP 4 constructor
      *
      * @since 1.0.0
      * @global object $wpdb
      */
-    function WPBANNERIZE_CLASS() {
+    function WPBannerizeClass($__file__) {
 		global $wpdb;
 
 		/**
@@ -112,13 +115,13 @@ class WPBANNERIZE_CLASS {
 		$splitVersion = explode(".", $this->version);
 		$this->release = $splitVersion[0];
 		$this->minor = $splitVersion[1];
-		$this->revision = $splitVersion[2];
+		$this->revision = isset($splitVersion[2]) ? $splitVersion[2] : '';
 		if( count( $splitVersion ) > 3) {
 			$this->minorRevision	= $splitVersion[3]; // @since 2.7.1.1
 		}
 
 		// Build the common and usefull path
-		$this->url = plugins_url("", __FILE__);
+		$this->url = plugins_url("", $__file__);
 
 		if (! defined('WP_CONTENT_DIR'))
 			define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
@@ -136,52 +139,44 @@ class WPBANNERIZE_CLASS {
 			define('WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins');
 
 		/**
-		 * Add $wpdb->prefix to table name define in WP_BANNERIZE_TABLE. This featured makes wp-bannerize compatible
+		 * Add $wpdb->prefix to table name define in kWPBannerizeTableName. This featured makes wp-bannerize compatible
 		 * with Wordpress MU and Wordpress with different database prefix
 		 *
 		 * @since 2.2.1
 		 */
-		$this->table_bannerize = $wpdb->prefix . WP_BANNERIZE_TABLE;
+		$this->table_bannerize = $wpdb->prefix . kWPBannerizeTableName;
 
 		/**
-		 * Conversion Old Database
+		 * Used for check previous 3.0 release database table name
 		 *
 		 * @since 2.5.0
 		 */
-		$this->old_table_bannerize = $wpdb->prefix . WP_BANNERIZE_TABLE_2411;
-		$this->prev_table_bannerize = $wpdb->prefix . WP_BANNERIZE_TABLE_250;
+		$this->prev_table_bannerize = $wpdb->prefix .kWPBannerizeTableName_3_0;
 
-		$this->path = dirname(__FILE__);
-		$this->file = basename(__FILE__);
+		$this->path = dirname($__file__);
+		$this->file = basename($__file__);
 		$this->directory = basename($this->path);
-		$this->uri = plugins_url("", __FILE__);
+		$this->uri = plugins_url("", $__file__);
 		$this->siteurl = get_bloginfo('url');
 		$this->wpadminurl = admin_url();
 
 		$this->content_url = get_option('siteurl') . '/wp-content';
-		$this->plugin_url = $this->content_url . '/plugins/' . plugin_basename(dirname(__FILE__)) . '/';
-		$this->ajax_sorter = $this->plugin_url . "ajax_sorter.php";
-		$this->ajax_clickcounter = $this->plugin_url . "ajax_clickcounter.php";
-	}
+		$this->plugin_url = $this->content_url . '/plugins/' . plugin_basename(dirname($__file__)) . '/';
 
-    /**
-     * Check the Wordpress relase for more setting
-     *
-     * @deprecated
-     */
-    function checkWordpressRelease() {
-        global $wp_version;
-        if ( strpos($wp_version, '2.7') !== false || strpos($wp_version, '2.8') !== false  ) {}
-    }
-} // end of class
+		// New Engine Ajax
+		$protocol = isset( $_SERVER['HTTPS'] ) ? 'https://' : 'http://';
+		$this->ajaxURL = admin_url( 'admin-ajax.php', $protocol );
+
+	}
+}
 
 /**
- * Avoid widget support
+ * Widget support
  *
  * @since 2.3.5
  */
-if(class_exists("WP_Widget")) {
-    require_once('wp-bannerize_widget.php');
-    add_action('widgets_init', create_function('', 'return register_widget("WP_BANNERIZE_WIDGET");'));
+if(class_exists("WP_Widget")) { 
+    require_once('wpBannerizeWidget.php');
+    add_action('widgets_init', create_function('', 'return register_widget("' . kWPBannerizeWidgetClassName . '");'));
 }
 ?>
